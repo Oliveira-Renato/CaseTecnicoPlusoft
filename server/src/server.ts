aa252e0ca1891dbf3ad828bc7bb36435a2834b81
Oliveira-Renato <renato.pritf@gmail.com>
2024-05-15 11:40:54 -0300

    config: eslint

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,12 @@ import fastify from "fastify";
 
 const app = fastify();
 
+app.get("/hello", () => {
+  return 'Hello World!'
+})
+
 app.listen({
-  port: 33333
+  port: 3333
 }).then(() => {
   console.log("🚀  HTTP server running on http://localhost:3333")
 })
